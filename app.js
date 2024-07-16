2aec2a7e627f67c6ecda5529a2694a8e5d7df99c
gitDaksh <daksh.java.util@gmail.com>
2024-07-16 16:46:34 +0530

    create listing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listings.js");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -17,6 +18,21 @@ async function main() {
 app.get("/", (req, res) => {
     res.send("root working!!");
 })
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "my new villa",
+        description: "by the beach",
+        price: 1200,
+        location: "Goa",
+        country: "India"
+    });
+
+    await sampleListing.save();
+
+    console.log("sample was saved");
+    res.send("successful");
+});
 
 app.listen(8080, () => {
     console.log("listening!");
--- models/listings.js
+++ models/listings.js
@@ -3,14 +3,19 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title: {
-
+        type: String, 
+        required: true
     },
     description: String,
-    image: String,
+    image: {
+        default : "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        type: String,
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+    },
     price: Number,
     location: String,
     country: String
 });
 
 const listing = mongoose.model("Listing", listingSchema);
-modules.export = listing;
+module.exports = listing;
