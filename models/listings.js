88fc84fe98f3a423015b8571e88f3b8dd42ba721
gitDaksh <daksh.java.util@gmail.com>
2024-07-20 00:11:32 +0530

    add all listings

--- models/listings.js
+++ models/listings.js
@@ -8,9 +8,12 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        default : "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        type: String,
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        filename: { type: String, default: "default_image" },
+        url: { 
+            type: String,
+            default: "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1717560620356-5b6603d13472?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        }
     },
     price: Number,
     location: String,
